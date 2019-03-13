fb66a7c57b21578ebb242a0cf41eb6f11d3700b3
ncowboy <latentpickuper@gmail.com>
2019-03-14 01:14:29 +0300

    Задания 1 и 2 готовы

--- lesson5/1-2/script.js
+++ lesson5/1-2/script.js
@@ -8,6 +8,9 @@
 
 "use strict";
 
+/**
+ * Функция создания шахматной доски
+ */
 function chessBoardRender() {
     //Создаем доску
     const board = document.createElement('table');
@@ -15,11 +18,19 @@ function chessBoardRender() {
     document.body.appendChild(board);
 
     //Создаем ячейки
+
+    //Создаем ряды
     for (let i = 0; i < 10; i++) {
         const row = document.createElement('tr');
-        row.classList.add('row');
+        if (i !== 0 && i !== 9) {  //Не добавляем класс ячейкам, в которых будет нумерация
+            row.classList.add('row');
+        }
         board.appendChild(row);
     }
+
+    // Создаем колонки в каждом ряду
+
+    //Даем разные классы ячейкам под нумерацию, фигуры и пустые (угловые)
 
     const rows = board.getElementsByTagName('tr');
     for (let k = 0; k < rows.length; k++) {
@@ -40,19 +51,84 @@ function chessBoardRender() {
     }
 
     // Заполняем буквы и цифры
-    const  letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
-    const colH = board.querySelectorAll('.col-header:nth-child(2n');
+    const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+    const colH = board.querySelectorAll('.col-header');
     const rowHLeft = board.querySelectorAll('.row-header:nth-child(1)');
-    const rowHRight = board.querySelectorAll('.row-header:nth-child(n+2)');
-    console.log(colH);
+    const rowHRight = board.querySelectorAll('.row-header:nth-child(n + 2)');
 
-    for (let g = 0; g < 8; g++) {
-        rowHLeft[g].textContent = 8 - g;
-        rowHRight[g].textContent = 8 - g;
+    for (let g = 0; g < colH.length; g++) {
+        if (g > 7) {
+            colH[g].textContent = letters[g - 8]
+        } else {
+            colH[g].textContent = letters[g];
+            rowHLeft[g].textContent = 8 - g;
+            rowHRight[g].textContent = 8 - g;
+        }
     }
 
+    // Красим ячейки
+
+    /**
+     * Красит нужные ячейки в темный цвет в зависимости от четности строк
+     * @param {boolean} isEven Четность строк
+     */
+    function paintCells(isEven) {
+        const rows = board.querySelectorAll(`.row:nth-child(${isEven ? 'even' : 'odd'})`);
+        for (let i = 0; i < rows.length; i++) {
+            const cols = rows[i].querySelectorAll(`.col:nth-child(${isEven ? 'odd' : 'even'})`);
+            for (let k = 0; k < cols.length; k++) {
+                cols[k].classList.add('col-dark');
+            }
+        }
+    }
+
+    paintCells(true);
+    paintCells(false);
 
 }
 
+/**
+ * Расставляет фигуры на шахматной доске
+ */
+function placeFigures() {
+
+    /**
+     * Расставляет старшие фигуры
+     * @param {int} row Номер строки в дереве элементов
+     * @param {string} color Цвет фигур
+     */
+    function placeSeniors(row, color) {
+        const seniorsRow = document.querySelector(`.row:nth-child(${row})`);
+        const seniorsCells = seniorsRow.querySelectorAll('.col');
+        seniorsCells[0].innerHTML = `<i class="fas fa-chess-rook fa-2x color-${color}"></i>`;
+        seniorsCells[7].innerHTML = `<i class="fas fa-chess-rook fa-2x color-${color}"></i>`;
+        seniorsCells[1].innerHTML = `<i class="fas fa-chess-knight fa-2x color-${color}"></i>`;
+        seniorsCells[6].innerHTML = `<i class="fas fa-chess-knight fa-2x color-${color}"></i>`;
+        seniorsCells[2].innerHTML = `<i class="fas fa-chess-bishop fa-2x color-${color}"></i>`;
+        seniorsCells[5].innerHTML = `<i class="fas fa-chess-bishop fa-2x color-${color}"></i>`;
+        seniorsCells[3].innerHTML = `<i class="fas fa-chess-king fa-2x color-${color}"></i>`;
+        seniorsCells[4].innerHTML = `<i class="fas fa-chess-queen fa-2x color-${color}"></i>`;
+    }
+
+    /**
+     * Расставляет пешки
+     * @param {int} row Номер строки в дереве элементов
+     * @param {string} color Цвет фигур
+     */
+
+    function placePawns(row, color) {
+        const pawnsRaw = document.querySelector(`.row:nth-child(${row})`);
+        const pawnsCells = pawnsRaw.querySelectorAll('.col');
+        pawnsCells.forEach((cell) =>
+            cell.innerHTML = `<i class="fas fa-chess-pawn fa-2x color-${color}"></i>`);
+    }
+
+    placeSeniors(2, 'black');
+    placeSeniors(9, 'white');
+    placePawns(3, 'black');
+    placePawns(8, 'white');
+
+}
 
 chessBoardRender();
+placeFigures();
